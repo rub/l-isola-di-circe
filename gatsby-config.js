5d33926c94c340481d4aa2454c4e0427e9f10564
Alessio Rapini <alessio.rapini@yahoo.it>
2024-04-27 17:30:55 +0200

    Setup PostCSS

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,19 @@
  */
 module.exports = {
   siteMetadata: {
+    title: `L'Isola di Circe`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  plugins: [
+    {
+      resolve: "gatsby-plugin-postcss",
+      options: {
+        postCssPlugins: [
+          require("postcss-nesting")({
+            edition: "2024-02", // Use the latest version of CSS nesting
+          }),
+        ],
+      },
+    },
+  ],
+};

2af00c1b3da754019bbcf006090659616dc1d5b6
Alessio Rapini <alessio.rapini@yahoo.it>
2024-04-27 23:49:27 +0200

    Handle font with Fontsource

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../../global_css/modern-normalize.css";
+import "../../global_css/base.css";
 
 const Layout = ({ children }) => {
   return <main>{children}</main>;

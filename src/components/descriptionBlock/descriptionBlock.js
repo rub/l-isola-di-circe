49319ac0364bd27f153c53e7f702c005503a2d20
Alessio Rapini <alessio.rapini@yahoo.it>
2024-05-01 14:10:40 +0200

    Create DescriptionBlock component

---
+++ src/components/descriptionBlock/descriptionBlock.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {
+  description,
+  wrapper,
+  information,
+  text,
+  media,
+} from "./descriptionBlock.module.css";
+
+const DescriptionBlock = ({ title, copy, children }) => (
+  <div className={description}>
+    <div className={wrapper}>
+      <div className={information}>
+        <h2>{title}</h2>
+        <p className={text}>{copy}</p>
+      </div>
+      <div className={media}>{children}</div>
+    </div>
+  </div>
+);
+
+export default DescriptionBlock;

83ee0c24f65d4e55ee30f196b2d291b1cf6b9ea7
Alessio Rapini <alessio.rapini@yahoo.it>
2024-05-19 10:27:16 +0200

    Use GatsbyImage to handle gallery pictures

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,13 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { StaticImage } from "gatsby-plugin-image";
+import { useStaticQuery, graphql } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import {
   gallery,
   wrapper,
@@ -9,96 +10,63 @@ import {
   title,
 } from "./gallery.module.css";
 
+// TODO: Use ::before to display the shadow behind pictures.
 const Gallery = () => {
+  const data = useStaticQuery(graphql`
+    query MyQuery {
+      allFile(
+        filter: {
+          sourceInstanceName: { eq: "images" }
+          relativePath: { glob: "gallery/*" }
+        }
+      ) {
+        edges {
+          node {
+            childImageSharp {
+              gatsbyImageData(
+                width: 300
+                placeholder: DOMINANT_COLOR
+                formats: [AUTO, WEBP, AVIF]
+              )
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const images = data.allFile.edges.map((edge) => {
+    const image = getImage(edge.node.childImageSharp);
+    return { gatsbyImage: image };
+  });
+
+  const columns = [
+    [images[0], images[1]], // 1st column: 1st and 2nd images
+    [images[2]], // 2nd column: 3rd image
+    [images[3], images[4]], // 3rd column: 4th and 5th images
+    [images[5], images[6]], // 4th column: 6th and 7th images
+    [images[7]], // 5th column: 8th image
+    [images[8], images[9]], // 6th column: 9th and 10th images
+  ].filter((column) => column[0]);
+
   return (
     <div className={gallery}>
       <div className={titleWrapper}>
         <h2 className={title}>Esplora il nostro appartamento</h2>
       </div>
       <div className={wrapper}>
-        <div className={gridColumn}>
-          <StaticImage
-            className={picture}
-            src={
-              "https://images.unsplash.com/photo-1529408632839-a54952c491e5?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            }
-            alt=""
-          />
-          <StaticImage
-            className={picture}
-            src={
-              "https://images.unsplash.com/photo-1598897451897-a1fb4356827a?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            }
-            alt=""
-          />
-        </div>
-        <div className={gridColumn}>
-          {/* <h2 className={title}>Esplora il nostro appartamento</h2> */}
-          <StaticImage
-            className={picture}
-            src={
-              "https://images.unsplash.com/photo-1529408632839-a54952c491e5?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            }
-            alt=""
-          />
-        </div>
-        <div className={gridColumn}>
-          <StaticImage
-            className={picture}
-            src={
-              "https://images.unsplash.com/photo-1529408632839-a54952c491e5?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            }
-            alt=""
-          />
-          <StaticImage
-            className={picture}
-            src={
-              "https://images.unsplash.com/photo-1598897451897-a1fb4356827a?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            }
-            alt=""
-          />
-        </div>
-        <div className={gridColumn}>
-          <StaticImage
-            className={picture}
-            src={
-              "https://images.unsplash.com/photo-1529408632839-a54952c491e5?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            }
-            alt=""
-          />
-          <StaticImage
-            className={picture}
-            src={
-              "https://images.unsplash.com/photo-1598897451897-a1fb4356827a?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            }
-            alt=""
-          />
-        </div>
-        <div className={gridColumn}>
-          <StaticImage
-            className={picture}
-            src={
-              "https://images.unsplash.com/photo-1529408632839-a54952c491e5?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            }
-            alt=""
-          />
-        </div>
-        <div className={gridColumn}>
-          <StaticImage
-            className={picture}
-            src={
-              "https://images.unsplash.com/photo-1529408632839-a54952c491e5?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            }
-            alt=""
-          />
-          <StaticImage
-            className={picture}
-            src={
-              "https://images.unsplash.com/photo-1598897451897-a1fb4356827a?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            }
-            alt=""
-          />
-        </div>
+        {columns.map((columnImages, columnIndex) => (
+          <div key={columnIndex} className={gridColumn}>
+            {columnImages.map((imageData, imageIndex) => (
+              <GatsbyImage
+                key={imageIndex}
+                className={picture}
+                image={imageData.gatsbyImage}
+                alt={`Gallery image ${columnIndex * 2 + imageIndex + 1}`}
+              />
+            ))}
+          </div>
+        ))}
       </div>
     </div>
   );
